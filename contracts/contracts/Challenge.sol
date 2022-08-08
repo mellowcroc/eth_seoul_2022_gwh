d3e29ce71404990298531475070fd0aa4945a4ac
jaehunkim <jaehunkim@outlook.com>
2022-08-08 21:41:46 +0900

    Implemented challenge contract & added tests

--- contracts/contracts/Challenge.sol
+++ contracts/contracts/Challenge.sol
@@ -4,36 +4,70 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract Challenge {
-    // TODO : CUSTOM QUORUM
-    uint256 public immutable quorum = 20;
+    enum ChallengeStatus {
+        Ongoing,
+        Disapproved,
+        Approved
+    }
 
-    address public challenger;
+    // TODO : CUSTOM QUORUM
+    uint256 public immutable quorumPermil = 200;
+    uint256 public immutable requiredApprovalRatioPermil = 600;
+    uint256 public immutable duration = 14 days;
+
     address public donation;
+    address public challenger;
     string public desc;
 
     uint256 public votableUntil;
 
     mapping(address => bool) public voted;
-    uint256 public maxVotes;
+    uint256 public maxVoter;
     uint256 public yesVotes;
     uint256 public noVotes;
 
-    bool public isClosed;
+    ChallengeStatus public status;
 
-    // TODO : CUSTOM START/END
-    constructor(
-        address challenger_,
-        address donation_,
-        string memory desc_
-    ) {
-        challenger = challenger_;
+    constructor(address donation_) {
         donation = donation_;
-        desc = desc_;
-        votableUntil = block.timestamp + 14 days;
     }
 
-    function getInfo() public view returns (address, address, string memory) {
-        return (challenger, donation, desc);
+    // TODO : CUSTOM START/END
+    function initialize(
+        address challenger_,
+        string memory desc_,
+        uint256 maxVoter_
+    ) public {
+        challenger = challenger_;
+        desc = desc_;
+        votableUntil = block.timestamp + duration;
+        status = ChallengeStatus.Ongoing;
+        maxVoter = maxVoter_;
+    }
+
+    function getInfo()
+        public
+        view
+        returns (
+            address,
+            address,
+            string memory,
+            uint256
+        )
+    {
+        return (challenger, donation, desc, votableUntil);
+    }
+
+    function getVoteInfo()
+        public
+        view
+        returns (
+            uint256,
+            uint256,
+            uint256
+        )
+    {
+        return (yesVotes, noVotes, maxVoter);
     }
 
     function getChallenger() public view returns (address) {
@@ -52,7 +86,35 @@ contract Challenge {
         return votableUntil;
     }
 
-    function vote(bool yn) public {}
+    function vote(bool yn) public {
+        require(block.timestamp < votableUntil, "Vote perioed has ended.");
+        // reqire if voter is user
+        require(!voted[msg.sender], "Already voted.");
+        voted[msg.sender] = true;
+        if (yn) {
+            yesVotes++;
+        } else {
+            noVotes++;
+        }
+    }
 
-    function closeChallenge() public {}
+    function closeChallenge() public {
+        require(block.timestamp > votableUntil, "Vote perioed has not ended.");
+        require(status == ChallengeStatus.Ongoing, "This challenge has ended.");
+
+        uint256 votePermil = (yesVotes * 1000) / maxVoter;
+        uint256 yesPermil = (yesVotes * 1000) / (yesVotes + noVotes);
+        if (
+            votePermil >= quorumPermil &&
+            yesPermil >= requiredApprovalRatioPermil
+        ) {
+            status = ChallengeStatus.Approved;
+        } else {
+            status = ChallengeStatus.Disapproved;
+        }
+    }
+
+    function getChallengeStatus() public view returns (ChallengeStatus) {
+        return status;
+    }
 }
--- contracts/test/test_challenge.ts
+++ contracts/test/test_challenge.ts
@@ -1,16 +1,120 @@
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { expect } from "chai";
 import { ethers } from "hardhat";
+import { Challenge, Challenge__factory } from "../typechain";
+
+enum ChallengeStatus {
+  Ongoing,
+  Disapproved,
+  Approved,
+}
 
 describe("Challenger", function () {
-  it("Should deploy challenge", async function () {
-    const [addr1, addr2] = await ethers.getSigners();
+  let challenger: SignerWithAddress;
+  let addr1: SignerWithAddress;
+  let addr2: SignerWithAddress;
+  let addr3: SignerWithAddress;
+  let addr4: SignerWithAddress;
+  let challenge: Challenge;
+  const MAXVOTER = 3;
 
+  before(async () => {
+    [challenger, addr1, addr2, addr3, addr4] = await ethers.getSigners();
+  });
+
+  it("Should deploy and initialize challenge", async function () {
     const Challenge = await ethers.getContractFactory("Challenge");
-    const challenge = await Challenge.deploy(
-      await addr1.getAddress(),
-      await addr2.getAddress(),
-      "somedesc"
-    );
+    challenge = await Challenge.deploy(await addr1.address);
+
     await challenge.deployed();
+
+    const ts = (await ethers.provider.getBlock("latest")).timestamp;
+    await challenge.initialize(challenger.address, "Simple vote", MAXVOTER);
+    expect(await challenge.getChallenger()).to.equal(challenger.address);
+    expect(await challenge.getDonation()).to.equal(addr1.address);
+    expect(await challenge.getDesc()).to.equal("Simple vote");
+    expect(await challenge.getChallengeStatus()).to.equal(
+      ChallengeStatus.Ongoing
+    );
+    const [yesVotes, noVotes, maxVoter] = await challenge.getVoteInfo();
+    expect(yesVotes).to.equal(0);
+    expect(noVotes).to.equal(0);
+    expect(maxVoter).to.equal(MAXVOTER);
+    // XXX : why +1 diff
+    expect(await challenge.getVotableUntil()).to.equal(
+      ts + 14 * 60 * 60 * 24 + 1
+    );
+  });
+
+  it("Users should vote, non-users shouldnt vote", async function () {
+    const challengeAsUser1 = Challenge__factory.connect(
+      challenge.address,
+      addr1
+    );
+
+    await challengeAsUser1.vote(true);
+    await expect(challengeAsUser1.vote(true)).to.revertedWith("Already voted.");
+
+    let [yesVotes, noVotes, maxVoter] = await challenge.getVoteInfo();
+    expect(yesVotes).to.equal(1);
+    expect(noVotes).to.equal(0);
+
+    const challengeAsUser2 = Challenge__factory.connect(
+      challenge.address,
+      addr2
+    );
+
+    await challengeAsUser2.vote(false);
+    await expect(challengeAsUser2.vote(false)).to.revertedWith(
+      "Already voted."
+    );
+
+    [yesVotes, noVotes, maxVoter] = await challenge.getVoteInfo();
+    expect(yesVotes).to.equal(1);
+    expect(noVotes).to.equal(1);
+
+    const challengeAsUser3 = Challenge__factory.connect(
+      challenge.address,
+      addr3
+    );
+
+    await challengeAsUser3.vote(true);
+    await expect(challengeAsUser3.vote(true)).to.revertedWith("Already voted.");
+
+    [yesVotes, noVotes, maxVoter] = await challenge.getVoteInfo();
+    expect(yesVotes).to.equal(2);
+    expect(noVotes).to.equal(1);
+  });
+
+  it("Users couldnt close challenge early", async function () {
+    await expect(challenge.closeChallenge()).to.revertedWith(
+      "Vote perioed has not ended."
+    );
+  });
+
+  it("Users couldnt vote late", async function () {
+    await ethers.provider.send("evm_increaseTime", [14 * 60 * 60 * 24 + 1000]);
+    await ethers.provider.send("evm_mine", []);
+
+    const challengeAsUser4 = Challenge__factory.connect(
+      challenge.address,
+      addr4
+    );
+
+    await expect(challengeAsUser4.vote(true)).to.revertedWith(
+      "Vote perioed has ended."
+    );
+  });
+
+  it("User close challenge", async function () {
+    await challenge.closeChallenge();
+
+    await expect(challenge.closeChallenge()).to.revertedWith(
+      "This challenge has ended."
+    );
+
+    expect(await challenge.getChallengeStatus()).to.equal(
+      ChallengeStatus.Approved
+    );
   });
 });
