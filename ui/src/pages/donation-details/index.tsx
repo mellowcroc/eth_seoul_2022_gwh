51027d5dfc0b1012dfa87802451184fc820006c2
mellowcroc <mellowcroc@gmail.com>
2022-08-08 16:02:15 +0900

    Add donation details page skeleton.

--- ui/src/pages/donation-details/index.tsx
+++ ui/src/pages/donation-details/index.tsx
@@ -1,3 +1,5 @@
+import { useEthers } from '@usedapp/core';
+import { useState } from 'react';
 import styled from 'styled-components';
 import { useQuery } from '../../hooks/useQuery';
 
@@ -5,8 +7,54 @@ const Wrapper = styled.div`
   text-align: center;
 `;
 
+const Id = styled.div``;
+
+const Name = styled.div``;
+
+const Description = styled.div``;
+
+const Stage = styled.div``;
+
+const Organization = styled.div``;
+
+const Whale = styled.div``;
+
+const MatchWrapper = styled.div``;
+const MatchAmount = styled.div``;
+const MatchPercent = styled.div``;
+const MatchExpireAt = styled.div``;
+const BountyPool = styled.div``;
+
+const UserDonationWrapper = styled.div``;
+const UserDonation = styled.div``;
+
+const ChallengeWrapper = styled.div``;
+const Challenge = styled.div``;
+
+const ReportWrapper = styled.div``;
+const Report = styled.div``;
+
 export default function DonationDetails() {
   const query = useQuery();
+  const donationId = query.get('id');
+  const { account } = useEthers();
+  // TODO(): get the actual data from ethereum
+  const [donation, setDonation] = useState({
+    id: donationId,
+    name: `Donation ${donationId}`,
+    stage: 'Funding',
+    description: 'Description..... blabalbalbalblablalbllab',
+    whaleAddress: '0xABCD...ABCD',
+    orgAddress: '0x1234...1234',
+    bountyPool: 1000,
+    matchAmount: 50000,
+    matchPercent: 100,
+    matchExpireAt: new Date('2022-09-08').getTime(),
+    userDonations: [],
+    matchedTotal: 5000,
+    challenges: [],
+    reports: [],
+  });
 
   return (
     <div>
@@ -14,25 +62,53 @@ export default function DonationDetails() {
         <h1>
           {`Donation Details of "${query.get('id')}"`}
         </h1>
-        {/* 
-        - id
-        - name
-        - description
-        - stage
-        - orgAddress
-        - whaleAddress
-        - matchAmount // matchAmountTotal?
-            - minimum 5,000 USDC
-        - bountyPool
-            - minimum 500 USDC
-        - matchPerc
-            - range: [10% ~]
-        - matchExpireAt
-        - mapping(address ⇒ uint256) userDonations
-        - matchedTotal
-        - challenges
-        - Report[] reports
-        */}
+
+        <Id>ID: {donation.id}</Id>
+
+        <Name>Name: {donation.name}</Name>
+
+        <Description>Description: {donation.description}</Description>
+
+        <Stage>Stage: {donation.stage}</Stage>
+
+        <Organization>Organization: {donation.orgAddress}</Organization>
+
+        <Whale>Whale: {donation.whaleAddress}</Whale>
+
+        <MatchWrapper>
+          Matching Info
+          <MatchAmount>Matching Amount: {donation.matchAmount}</MatchAmount>
+          <MatchPercent>Matching Percentage: {donation.matchPercent}</MatchPercent>
+          <MatchExpireAt>Matching Expires At: {donation.matchExpireAt}</MatchExpireAt>
+          <BountyPool>Bounty Pool: {donation.bountyPool}</BountyPool>
+        </MatchWrapper>
+
+        <UserDonationWrapper>
+          User Donations
+          {
+            donation.userDonations
+              .map((userDonation, index) =>
+                <UserDonation key={`user-donation-${index}`}>{userDonation}</UserDonation>)
+          }
+        </UserDonationWrapper>
+
+        <ChallengeWrapper>
+          Challenges
+          {
+            donation.challenges
+              .map((challenge, index) =>
+                <Challenge key={`challenge-${index}`}>{challenge}</Challenge>)
+          }
+        </ChallengeWrapper>
+
+        <ReportWrapper>
+          Reports
+          {
+            donation.reports
+              .map((report, index) =>
+                <Report key={`report-${index}`}>{report}</Report>)
+          }
+        </ReportWrapper>
       </Wrapper>
     </div>
   );
