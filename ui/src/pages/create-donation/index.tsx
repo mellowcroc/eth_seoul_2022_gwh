5c45d9dcac0a752e1c62170258658c21443a4c69
jaehunkim <jaehunkim@outlook.com>
2022-08-11 07:20:35 +0900

    Connect create button

--- ui/src/pages/create-donation/index.tsx
+++ ui/src/pages/create-donation/index.tsx
@@ -1,8 +1,30 @@
-import { useEthers } from '@usedapp/core';
-import { useState } from 'react';
-import styled from 'styled-components';
-import Connect from '../../components/Connect';
-import Header from '../../components/Header';
+import { useEthers } from "@usedapp/core";
+import { useState, useMemo } from "react";
+import styled from "styled-components";
+import Connect from "../../components/Connect";
+import Header from "../../components/Header";
+import { useDonation } from "../../hooks/useDonation";
+import { DONATION_FACTORY, USDC_ADDRESS } from "../../contracts";
+import { BigNumber, Contract } from "ethers";
+import {
+  donationAbi,
+  donationFactoryAbi,
+  challengeAbi,
+  erc20Abi,
+} from "../../abis";
+import {
+  USDC,
+  Challenge__factory,
+  Challenge,
+  DonationFactory,
+  DonationFactory__factory,
+  Donation,
+  Donation__factory,
+} from "contracts";
+
+function convertTo18Decimals(num: number) {
+  return BigNumber.from(num).mul(BigNumber.from(10).pow(18));
+}
 
 const Container = styled.div`
   display: flex;
@@ -46,21 +68,66 @@ const StyledInput = styled("input")`
   font-weight: 500;
 `;
 
-const CreateButton = styled.button`
-
-`;
-
-const _handleCreateDonation = async () => {}
+const CreateButton = styled.button``;
 
 export default function CreateDonation() {
-  const { account } = useEthers();
-  const [name, setName] = useState('');
-  const [description, setDescription] = useState('');
-  const [org, setOrg] = useState('');
-  const [matchPool, setMatchPool] = useState(0);
-  const [bountyPool, setBountyPool] = useState(0);
-  const [matchPerc, setMatchPerc] = useState(0);
-  const [matchExpireAt, setMatchExpireAt] = useState(0);
+  const { account, library } = useEthers();
+  const [name, setName] = useState("Name1");
+  const [description, setDescription] = useState("Desc1");
+  const [org, setOrg] = useState("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266");
+  const [matchPool, setMatchPool] = useState(5000);
+  const [bountyPool, setBountyPool] = useState(500);
+  const [matchPerc, setMatchPerc] = useState(50);
+  const [matchExpireAt, setMatchExpireAt] = useState(3600);
+  const { donations, myDonations } = useDonation(DONATION_FACTORY, "");
+
+  const signer = useMemo(() => library?.getSigner(), [library]);
+  const factory = useMemo<DonationFactory | undefined>(
+    () =>
+      signer
+        ? (
+          new Contract(
+            DONATION_FACTORY,
+            donationFactoryAbi,
+            library
+          ) as DonationFactory
+        ).connect(signer)
+        : undefined,
+    [signer, library]
+  );
+  const usdc = useMemo<USDC | undefined>(
+    () =>
+      signer
+        ? (new Contract(USDC_ADDRESS, erc20Abi, library) as USDC).connect(
+          signer
+        )
+        : undefined,
+    [signer, library]
+  );
+
+  const _handleCreateDonation = async () => {
+    if (!factory || !usdc || !account) {
+      alert("Connect wallet first");
+      return;
+    }
+
+    console.log("Allowance : ", matchPool + bountyPool);
+    const tx = await usdc.approve(
+      DONATION_FACTORY,
+      convertTo18Decimals(matchPool + bountyPool + 1000)
+    );
+    await tx.wait(1);
+    console.log("MatchPool", matchPool);
+    await factory.createWhaleDonation(
+      name,
+      description,
+      org,
+      convertTo18Decimals(matchPool),
+      matchPerc,
+      convertTo18Decimals(bountyPool),
+      matchExpireAt
+    );
+  };
 
   return (
     <Container>
@@ -68,76 +135,68 @@ export default function CreateDonation() {
         <Header />
       </HeaderContainer>
       <Content>
-        <h1>
-          Create a new donation
-        </h1>
+        <h1>Create a new donation</h1>
 
         {/* Create form */}
-        { account && (
+        {account && (
           <>
             <StyledInput
               value={name}
-              placeholder='Name'
+              placeholder="Name"
               onChange={(event) => {
                 setName(event.target.value);
               }}
             />
             <StyledInput
               value={description}
-              placeholder='Description'
+              placeholder="Description"
               onChange={(event) => {
                 setDescription(event.target.value);
               }}
             />
             <StyledInput
               value={org}
-              placeholder='Charity Organization'
+              placeholder="Charity Organization"
               onChange={(event) => {
                 setOrg(event.target.value);
               }}
             />
-            <StyledInput
-              placeholder='Whale'
-              value={account}
-            />
+            <StyledInput placeholder="Whale" value={account} />
             <StyledInput
               value={matchPool}
-              placeholder='Match Pool'
+              placeholder="Match Pool"
               onChange={(event) => {
                 setMatchPool(Number(event.target.value));
               }}
             />
             <StyledInput
               value={bountyPool}
-              placeholder='Bounty Pool'
+              placeholder="Bounty Pool"
               onChange={(event) => {
                 setBountyPool(Number(event.target.value));
               }}
             />
             <StyledInput
               value={matchPerc}
-              placeholder='Match Percentage'
+              placeholder="Match Percentage"
               onChange={(event) => {
                 setMatchPerc(Number(event.target.value));
               }}
             />
             <StyledInput
               value={matchExpireAt}
-              placeholder='Matching Expiration'
+              placeholder="Matching Expiration"
               onChange={(event) => {
                 setMatchExpireAt(Number(event.target.value));
               }}
             />
 
-            <CreateButton onClick={_handleCreateDonation}>
-              Create!
-            </CreateButton>
-          </>) }
+            <CreateButton onClick={_handleCreateDonation}>Create!</CreateButton>
+          </>
+        )}
 
         {/* Connect Wallet Popup */}
-        {
-          !account && <Connect />
-        }
+        {!account && <Connect />}
       </Content>
     </Container>
   );
